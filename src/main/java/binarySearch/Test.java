5916516cda148f0c9119bc407c9d12ff3c2d04c7
Achilles <2832468078@qq.com>
2023-07-03 16:39:40 +0800

    LeetCode 153 154

--- src/main/java/binarySearch/Test.java
+++ src/main/java/binarySearch/Test.java
@@ -7,8 +7,12 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 public class Test {
     public static void main(String[] args) {
-        int[] arr1={1,5,3,6,7};
-        int[] arr2={1,3,2,4};
-        System.out.println(new L1187Solution().makeArrayIncreasing(arr1, arr2));
+//        L153Solution solution=new L153Solution();
+//        int[] data={3,1,2};
+//        System.out.println(solution.findMin(data));
+        L154Solution solution=new L154Solution();
+        int[] data={2,2,2,0,0,1};
+        System.out.println(solution.findMin(data));
+
     }
 }
